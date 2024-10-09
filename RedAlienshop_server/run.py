0fdc4a43120d798df24409dd12bc57450535e1f4
RedAlien <whdrjs5855@naver.com>
2024-10-09 20:38:21 +0900

    main

--- RedAlienshop_server/run.py
+++ RedAlienshop_server/run.py
@@ -21,7 +21,7 @@ logo = """
 
 if __name__ == "__main__":
   print(logo)
-  flask_app.run(host="0.0.0.0",debug=False, threaded=True, port=8080 )
+  flask_app.run(host="0.0.0.0",debug=False, threaded=True, port=8090 )
   
 # db 오류 발생할 때, 먼저 DB를 생성 했는지 체크할 것
 # migration 디렉토리 + db파일이 이미 생성되어 있다면 삭제하고 다시 아래 DB생성 과정 수행해야 한다
