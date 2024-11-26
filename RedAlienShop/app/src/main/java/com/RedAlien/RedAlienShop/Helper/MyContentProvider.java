4fb8395aa7b807f7cb9ba359d97e26cc093a9568
RedAlien <whdrjs5855@naver.com>
2024-11-26 13:06:07 +0900

    Update MyContentProvider

--- RedAlienShop/app/src/main/java/com/RedAlien/RedAlienShop/Helper/MyContentProvider.java
+++ RedAlienShop/app/src/main/java/com/RedAlien/RedAlienShop/Helper/MyContentProvider.java
@@ -10,6 +10,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MyContentProvider extends ContentProvider {
     private static final String TAG = "MyContentProvider";
     private static final String AUTHORITY ="com.RedAlien.provider";
@@ -68,6 +75,8 @@ public class MyContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
+        ifDisallowedPatternMatches( contentValuesToStringArray(values) );
+
         long rowID;
         Uri _uri;
         String tableName;
@@ -108,8 +117,25 @@ public class MyContentProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder(); // db 쿼리문을 쉽게 작성할 수 있도록 도와주는 유틸리티 클래스
+        List<String> myList = new ArrayList<>();
+
+        if (projection != null){
+            myList.addAll(Arrays.asList(projection));
+        }
+        if (selection != null){
+            myList.add(selection);
+        }
+        if (selectionArgs != null){
+            myList.addAll(Arrays.asList(selectionArgs));
+        }
+        if (sortOrder != null){
+            myList.add(sortOrder);
+        }
+        ifDisallowedPatternMatches(myList.toArray(new String[0]));
+
+        // db 쿼리문을 쉽게 작성할 수 있도록 도와주는 유틸리티 클래스
         // 기존 SQLiteDatabase는 한 개 테이블만 쿼리문을 작성할 수 있으나, 이것을 사용할 경우, 여러 테이블에 대한 쿼리문 실행 가능
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
         switch (uriMatcher.match(uri)){
             case USER:
@@ -139,6 +165,20 @@ public class MyContentProvider extends ContentProvider {
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        List<String> list = new ArrayList<>();
+
+        if (values != null){
+            String[] array = contentValuesToStringArray(values);
+            list.addAll(Arrays.asList(array));
+        }
+        if (selection != null){
+            list.add(selection);
+        }
+        if (selectionArgs != null){
+            list.addAll(Arrays.asList(selectionArgs));
+        }
+        ifDisallowedPatternMatches(list.toArray(new String[0]));
+
         int count; // update로 영향받은 row수
         String tableName;
 
@@ -168,6 +208,16 @@ public class MyContentProvider extends ContentProvider {
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
+        List<String> list = new ArrayList<>();
+
+        if (selection != null){
+            list.add(selection);
+        }
+        if (selectionArgs != null){
+            list.addAll(Arrays.asList(selectionArgs));
+        }
+        ifDisallowedPatternMatches(list.toArray(new String[0]));
+
         int count; // delete로 영향을 받은 행수
         String tableName;
 
@@ -193,5 +243,29 @@ public class MyContentProvider extends ContentProvider {
         count = db. delete(tableName, selection, selectionArgs);
         getContext().getContentResolver().notifyChange(uri, null);
         return count;
+    }
+
+    public String[] contentValuesToStringArray(ContentValues values){
+        String[] result = new String[values.size()];
+        int index = 0;
+
+        for (Map.Entry<String,Object> entry : values.valueSet()){
+            result[index] = String.valueOf(entry.getValue());
+            index++;
+        }
+        return result;
+    }
+
+    public void ifDisallowedPatternMatches(String[] array){
+        String regex = "([*-;#])|SQLITE_MASTER|AND|OR|INSERT|SELECT|UPDATE|DELETE|UNION";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+        Matcher matcher;
+
+        for (String element : array){
+            matcher = pattern.matcher(element);
+            if(matcher.find()){
+                throw new SecurityException("Contains disallowedCharacters : " +  matcher.group());
+            }
+        }
     }
 }
